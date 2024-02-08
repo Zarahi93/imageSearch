0eeddff197a866ef2d9e0c5c95c8df216a8da117
Zarahi93 <adahi.g.z@hotmail.com>
2024-02-07 21:21:41 -0600

    Deleting the previous results so they don't show up with the results of the new search

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ async function imagesSearch(){
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
     const response = await fetch(url) ;
     const data = await response.json();
+    if(page === 1){
+       searchResult.innerHTML = "";
+    };
     const results = data.results;
 
     results.map((results)=>{
