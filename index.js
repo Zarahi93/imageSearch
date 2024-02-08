462ef062405862af8f55d667ddad65f048613e92
Zarahi93 <adahi.g.z@hotmail.com>
2024-02-07 21:07:23 -0600

    Adding the event listener to the showMorebtn

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const accessKey = "bM30GSuCH9dSSIlvRl1o2Y6cMp--tmQejIcJC5ucssc";
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
-const searchMoreBtn = document.getElementById("show-more-btn");
+const showMoreBtn = document.getElementById("show-more-btn");
 
 let keyword = "";
 let page = 1;
 
 async function imagesSearch(){
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
     const response = await fetch(url) ;
     const data = await response.json();
     const results = data.results;
+
     results.map((results)=>{
         const image = document.createElement("img");
         image.src = results.urls.small;
@@ -21,11 +22,18 @@ async function imagesSearch(){
         imageLink.target = "_blank";
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
-    })
+    });
+
+    showMoreBtn.style.display = "block";
 };
 
 searchForm.addEventListener("submit", (e)=>{
     e.preventDefault();
     page = 1;
+    imagesSearch();
+})
+
+showMoreBtn.addEventListener("click",()=>{
+    page++;
     imagesSearch();
 })
