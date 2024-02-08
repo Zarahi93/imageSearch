7d7ea3cc4bad8f0b893e86ae4762e7d89cd28c10
Zarahi93 <adahi.g.z@hotmail.com>
2024-02-07 21:00:36 -0600

    Creating the script, get and display the api respond

---
+++ index.js
@@ -0,0 +1,31 @@
+const accessKey = "bM30GSuCH9dSSIlvRl1o2Y6cMp--tmQejIcJC5ucssc";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function imagesSearch(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const response = await fetch(url) ;
+    const data = await response.json();
+    const results = data.results;
+    results.map((results)=>{
+        const image = document.createElement("img");
+        image.src = results.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = results.links.html;
+        imageLink.target = "_blank";
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+};
+
+searchForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    imagesSearch();
+})
